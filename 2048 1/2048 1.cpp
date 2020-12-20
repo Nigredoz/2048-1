add277c68415832828d75a9c8cbdc031fd7920ff
Никита Билле <billenikita@mail.ru>
2020-12-20 23:55:07 +0700

    1

---
+++ 2048 1/2048 1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std; 
+
+int board[4][4];
+int dirline[] = { 1, 0, -1, 0 };
+int dirColumn[] = { 0, 1, 0, -1 };
+
+void newGame() {
+	for (int i = 0; i < 4; ++i)
+		for (int j = 0; j < 4; ++j)
+			board[i][j] = 0;
+}
+
+void printUI() {
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) 
+			if (board[i][j] == 0)
+				cout << " ";
+			else
+				cout << board[i][j];
+			cout << "\n";
+		}
+	cout << "n:new game, w: up, s: down, d: right, a: left, q: quit";
+}
+
+int main()
+{
+	char commandToDir[128];
+	commandToDir['s'] = 0;
+	commandToDir['d'] = 1;
+	commandToDir['w'] = 2;
+	commandToDir['a'] = 3;
+	while (true) {
+		printUI();
+		char command; 
+		cin >> command;
+		if (command == 'n')
+			newGame();
+		else if (command == 'q')
+			break;
+		else {
+			int currentDirection = commandToDir[command];
+			cout << currentDirection << "\n";
+		}
+	}
+	return 0;
+}
